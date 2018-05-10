9c078fa263c3eb06eb9b285c37fec2a6eb011102
Ge Yang <yangge1987@gmail.com>
2018-05-10 11:18:05 -0700

    Fix tag_instance

--- jaynes/helpers.py
+++ jaynes/helpers.py
@@ -13,7 +13,7 @@ def get_temp_dir():
 
 
 def tag_instance(Name=None, **kwargs):
-    from jaynes.shell import ck
-    cmd = f"aws ec2 create-tags --resources $EC2_INSTANCE_ID --tags Key=Name,Value={Name} --region us-west-2"
+    from .shell import ck
+    cmd = f"aws ec2 create-tags --resources $EC2_INSTANCE_ID --tags Key=Name,Value='{Name}' --region us-west-2"
     # f"aws ec2 create-tags --resources $EC2_INSTANCE_ID --tags Key=exp_prefix,Value={instance_tag} --region us-west-2"
     return ck(cmd)
