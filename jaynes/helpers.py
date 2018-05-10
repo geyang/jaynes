400b10cf5580db69bf80c4a4bc8dc54f52179fed
Ge Yang <yangge1987@gmail.com>
2018-05-10 13:20:32 -0700

    WIP: fix tag

--- jaynes/helpers.py
+++ jaynes/helpers.py
@@ -16,7 +16,7 @@ def tag_instance(Name=None, region="us-west-2", **kwargs):
     from .shell import ck
     template = f"aws ec2 create-tags --resources $EC2_INSTANCE_ID --tags Key='{{key}}',Value='{{value}}' --region {region}"
     if Name:
-        kwargs.update(dict(Name=Name))
+        kwargs.update(Name=Name)
     cmd = '''EC2_INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`" ''' + \
           "".join([template.format(key=k, value=v) for k, v in kwargs.items()])
 
