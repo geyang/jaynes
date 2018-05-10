6091c3105e397ebd372d89f55a09f37446fe784d
Ge Yang <yangge1987@gmail.com>
2018-05-10 12:38:28 -0700

    Fix ck print

--- jaynes/shell.py
+++ jaynes/shell.py
@@ -3,13 +3,19 @@ import subprocess
 
 def c(cmd, *args, verbose=False, **kwargs):
     if verbose:
-        print(cmd, *args)
+        if args:
+            print(cmd, *args)
+        else:
+            print(cmd)
     subprocess.call(cmd, *args, **kwargs)
 
 
 def ck(cmd, *args, verbose=False, **kwargs):
     if verbose:
-        print(cmd, *args)
+        if args:
+            print(cmd, *args)
+        else:
+            print(cmd)
     try:
         subprocess.check_call(cmd, *args, **kwargs)
     except subprocess.CalledProcessError as e:
