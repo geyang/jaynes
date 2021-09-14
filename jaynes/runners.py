8b881c85dde2d814ce6baed57145d530c24e3c26
Ge Yang <yangge1987@gmail.com>
2021-09-14 00:01:56 -0400

    remove log file and additional sbatch configs

--- jaynes/runners.py
+++ jaynes/runners.py
@@ -147,13 +147,13 @@ class Slurm(RunnerType):
             NOTE: It's quite hard to run `tail -f` on all the logfiles in the proper order,
             thus it is only applied to the first job.
             """
-            logfile = f"{work_dir}/slurm-%j.out"
-            sbatch_options = (f"--output {logfile}", f"--error {logfile}")
-            sbatch_options = "\n".join(["#SBATCH " + opt for opt in sbatch_options])
+            # logfile = f"{work_dir}/slurm-%j.out"
+            # sbatch_options = (f"--output {logfile}", f"--error {logfile}")
+            # sbatch_options = "\n".join(["#SBATCH " + opt for opt in sbatch_options])
             sbatch_cmd = ""
             for i in range(n_seq_jobs):
                 sbatch_cmd += f"sbatch {option_str} {extra_options} -d singleton" \
-                              f"<<<'#!/bin/bash\n{sbatch_options}\n{cmd}\n{entry_env} {entry_script}'"
+                              f"<<<'#!/bin/bash\n{cmd}\n{entry_env} {entry_script}'"
 
             # Note: The tailing leave Ghost processes running on the login node, which eventually
             #   max-outs the number of processes in the system. We remove this support because
