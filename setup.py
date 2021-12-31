5cfdc9b0db3b1b484a25fbac203839fb66d26c40
Ge Yang <yangge1987@gmail.com>
2021-12-30 20:34:58 -0500

    update install_requires

--- setup.py
+++ setup.py
@@ -7,22 +7,28 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README'), encoding='u
 with open(path.join(path.abspath(path.dirname(__file__)), 'VERSION'), encoding='utf-8') as f:
     version = f.read()
 
-setup(name="jaynes",
-      description="A tool for running python code with runner on aws",
-      long_description=long_description,
-      version=version,
-      url="https://github.com/episodeyang/jaynes",
-      author="Ge Yang",
-      author_email="yangge1987@gmail.com",
-      license=None,
-      keywords=["jaynes", "logging", "DEBUG", "debugging", "timer",
-                "timeit", "decorator", "stopwatch", "tic", "toc",
-                ],
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          "Intended Audience :: Science/Research",
-          "Programming Language :: Python :: 3"
-      ],
-      packages=find_packages(),
-      install_requires=["aiofile"]
-      )
+setup(
+    name="jaynes",
+    description="A tool for running python code with runner on aws",
+    long_description=long_description,
+    version=version,
+    url="https://github.com/episodeyang/jaynes",
+    author="Ge Yang",
+    author_email="yangge1987@gmail.com",
+    license=None,
+    keywords=["jaynes", "logging", "DEBUG", "debugging", "timer",
+              "timeit", "decorator", "stopwatch", "tic", "toc",
+              ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3"
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "aiofile",
+        "cloundpickle==1.3.0",
+        "functional_notations",
+        "params-proto"
+    ]
+)
